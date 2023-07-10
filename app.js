a7cc1c225ce91bf2b5bca883a972f8b8429afed8
Abhinav <abhin.srh@gmail.com>
2023-07-10 19:21:52 +0530

    commit2

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ app.use(bodyParser.json());
 //     }
 // })
 
-
+ app.use((req, res, next) => {
+     res.header('Access-Control-Allow-Origin', '*');
+     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+     if(req.method==='OPTION'){
+         res.header('Access-Control-Allow-Method', 'PUT', 'POST', 'DELETE', 'PATCH', 'GET');
+         return res.status(200).json({}); 
+     }
+     next();
+ })
 app.use('/products', productRoutes);
 app.use('/order', orderRoutes);
 
